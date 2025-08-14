0b09b839b31997632c5e49cb06dfd29aeb44b292
marce <marcelacamposm2@gmail.com>
2025-08-14 17:57:48 -0300

    feat: add page components for navigation

---
+++ src/app/pages/experiencias/experiencias.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Experiencias } from './experiencias';
+
+describe('Experiencias', () => {
+  let component: Experiencias;
+  let fixture: ComponentFixture<Experiencias>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [Experiencias]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Experiencias);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/pages/experiencias/experiencias.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-experiencias',
+  imports: [],
+  templateUrl: './experiencias.html',
+  styleUrl: './experiencias.scss'
+})
+export class Experiencias {
+
+}
